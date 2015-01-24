d5f511ced1ebed61a91f194550bccae43bdb40fd
joelschum@gmail.com <joelschum@gmail.com>
2015-01-24 03:19:53 +0100

    Adaptive zoom level (dependant on player distance)

--- update.lua
+++ update.lua
@@ -1,11 +1,18 @@
 function updateGame()
-		-- Move and scale camera so that all players are in view
-	local targetTransformPos = {0,0}
+	-- Move and scale camera so that all players are in view
+	local xMin, xMax, yMin, yMax = math.huge, -math.huge, math.huge, -math.huge
 	for playerId = 1, #players do
-		targetTransformPos = vadd(targetTransformPos, vmul(players[playerId].position, 1/#players))
+		local player = players[playerId]
+		xMin, yMin = math.min(player.position[1], xMin), math.min(player.position[2], yMin)
+		xMax, yMax = math.max(player.position[1], xMax), math.max(player.position[2], yMax)
 	end
+	local defaultZoom = 0.4
+	local xScale = xRes / (xMax - xMin + xRes * defaultZoom)
+	local yScale = yRes / (yMax - yMin + yRes * defaultZoom)
+	
+	local targetTransformPos = {(xMin + xMax)/ 2.0, (yMin + yMax)/2.0}
 	local camPosRel = vsub(targetTransformPos, camera.position)
-	local targetTransformScale = 1.0
+	local targetTransformScale = math.min(1.0, xScale, yScale)
 	camera.position = vadd(camera.position, vmul(camPosRel, 3.0 * simulationDt))
 	camera.scale = camera.scale + (targetTransformScale - camera.scale) * 3.0 * simulationDt
 	
